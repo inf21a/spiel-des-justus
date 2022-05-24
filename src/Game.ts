f07c5e1d8c4f753eaf8cd7a3b9ba933b25a0e66d
Samuel Pasieka <samuel-p@outlook.de>
2022-05-24 15:14:30 +0200

    Added parts of game-state setup

--- src/Game.ts
+++ src/Game.ts
@@ -2,4 +2,14 @@ import type { Game } from "boardgame.io";
 
 export interface JustusGameState {}
 
-export const JustusGame: Game<JustusGameState> = {};
+export const JustusGame: Game<JustusGameState> = {
+    name:"Spiel des Justus",
+    minPlayers: 3,
+    maxPlayers: 6,
+
+    setup: () => ({fields: Array(47).fill(null)}),
+    moves: {
+
+    }
+
+};
